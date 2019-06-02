637e473a92a776233630e099d1856958f4fecb03
Shubhamk617 <30970403+Shubhamk617@users.noreply.github.com>
2019-06-03 00:31:39 +0530

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,4 @@
+function transfer(){
+    var p=document.getElementById("pi");
+    p.innerHTML="Transaction Successfully done and updated."
+}
---
+++ showTransactions.php
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<! DOCTYPE html PUBLIC-"//W3C//DTD XHTML 1.1//EN"
+<head>
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+    <title>Credit Management</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body >
+    <div class="bgimage">
+        <div class="menu"> <h3>EMPLOYEE MANAGEMENT SYSTEM</h3></div>
+        <?php 
+        $conn=mysqli_connect("localhost", "root", "") or die (mysql_error());
+        mysqli_select_db($conn,"employee") or die(mysql_error());
+
+         $res=mysqli_query($conn,"select * from transhistory");
+        
+         echo "<table border='5' class='tab'>";
+         echo "<tr> <th> SSN</th> <th> TRANSACTION FROM</th><th>TRANSACTION TO</th><th> CREDITS TRANSFERRED</th></tr>";
+         while($arr=mysqli_fetch_row($res))
+            echo "<tr><td>$arr[0]</td>
+                <td>$arr[1]</td>
+                <td>$arr[2]</td>
+                <td>$arr[3]</td>
+                 </tr>";
+         
+         echo "</table>";
+    mysqli_close($conn);
+    ?>
+    
+  
+    </div>
+</body>
+</html>
